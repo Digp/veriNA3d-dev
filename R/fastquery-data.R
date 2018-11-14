6c568183dc26bf3285730140d9a692b8850ea7c8
Digp <diego.gallego@irbbarcelona.org>
2018-11-14 18:52:32 +0000

    Modified the data documentation

--- R/fastquery-data.R
+++ R/fastquery-data.R
@@ -1,21 +1,21 @@
 #' List of NA containing PDB IDs and related data
 #'
-#' Data from particularly slow queries.
+#' Presaved data to speed up some queries.
 #'
 #' @docType data
 #'
 #' @usage data(fastquery)
 #'
-#' @format An object of class data.frame:
+#' @format An object of class data.frame with the following fields:
 #' \describe{
-#'   \item{pdbID}{PDB ID.}
-#'   \item{Technique}{Experimental technique.}
-#'   \item{Resol}{Resolution. For NMR structures it contains an empty string.}
-#'   \item{DNAclass}{Output of classifyDNA function}
-#'   \item{RNAclassOver0}{Output of classifyRNA with length=0 or length=1. If
+#'   \item{pdbID:}{PDB ID.}
+#'   \item{Technique:}{Experimental technique.}
+#'   \item{Resol:}{Resolution. For NMR structures it contains an empty string.}
+#'   \item{DNAclass:}{Output of classifyDNA function}
+#'   \item{RNAclassOver0:}{Output of classifyRNA with length=0 or length=1. If
 #'                           the structure has one or two nucleotides, it is 
 #'                           also considered an RNA containing structure.}
-#'   \item{RNAclassOver2}{Output of classifyRNA with length=3. RNA molecules
+#'   \item{RNAclassOver2:}{Output of classifyRNA with length=3. RNA molecules
 #'                           shorter than 3 are classified as NoRNA.}
 #' }
 #'
