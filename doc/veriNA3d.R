cdbac5b694c885ebe9fd5c1f1bcfe72111df07f8
Digp <diego.gallego@irbbarcelona.org>
2018-11-19 19:04:33 +0100

    Started playing with the vignettes

---
+++ doc/veriNA3d.R
@@ -0,0 +1,13 @@
+## ----setup, include = FALSE------------------------------------------------
+
+knitr::opts_chunk$set(
+    collapse   = TRUE,
+    comment    = "#>",
+    message    = FALSE,
+    warning    = FALSE,
+    fig.width  = 6,
+    fig.height = 2,
+    fig.wide   = TRUE
+)
+
+
