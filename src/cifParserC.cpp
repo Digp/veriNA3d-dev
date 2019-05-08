968ba85386b24847497cc7e6e8be2d1739785331
Digp <diego.gallego@irbbarcelona.org>
2019-05-08 13:57:21 +0200

    Improving the design of the helper function that detects new sections, allowing it to move back the file pointer if it returns false (0)

--- src/cifParserC.cpp
+++ src/cifParserC.cpp
@@ -9,21 +9,37 @@ using namespace Rcpp;
  */
 
 // Temporal macro definition
-#define maxchar 100
+#define maxchar 10000
 
 // Helper function to detect new mmCIF section based on lines like '# \n'
+// Designed to be executed after a \n character or beggining of file
 int newsec(FILE *file, int c)
 {
+    // Read first character
+    c = fgetc(file);
+    // Detect # character or move back file pointer 1 place
     if (c == '#') {
+        // Read second character
         c = fgetc(file);
+        // Detect space character or move back file pointer 2 places
         if (c == ' ') {
+            // Read third character
             c = fgetc(file);
+            // Detect new line of move back file pointer 3 places
             if (c == '\n') {
+                // Return 1 to be used as bool true
                 return 1;
-            }   
-        }   
+            } else {
+                fseek(file, -2, SEEK_CUR);
+            }
+        } else {
+            fseek(file, -2, SEEK_CUR);
+        }
+    } else {
+        fseek(file, -1, SEEK_CUR);
     }
 
+    // Return 0 to be used as bool false
     return 0;
 }
     
@@ -42,24 +58,31 @@ List cifParserC(std::string strings="")
     // Define object line and index to save the contents of the file
     char line[maxchar];
     int index = 0;
-    int newsection = 0;
+    int newsection = 1;
 
     // Iterate over the characters of the file
     for (int c = fgetc(file); c != EOF; c = fgetc(file))
     {
-        newsection = newsec(file, c);
 
         // Parse section
         if (newsection) 
         {
-            c = fgetc(file);
-            //printf("%c", c);
-            if (index < maxchar)
+            line[index] = c;
+            index++;
+            while ((c = fgetc(file)) != EOF && c != '\n')
             {
-                line[index] = 'q';
-                index++;
+                //c = fgetc(file);
+                //printf("%c", c);
+                if (index < maxchar)
+                {
+                    line[index] = c;
+                    index++;
+                }
             }
         }
+
+        // After newline, check if a new section starts
+        newsection = newsec(file, c);
     }
 
     // Terminate array
