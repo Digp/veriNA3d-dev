b2a23a96013a5f21b0cc211f480cd5c3ce41c756
Digp <diego.gallego@irbbarcelona.org>
2019-05-15 09:41:36 +0200

    Started developing a new helper function to parse the first section (entry) of the mmCIFs, the easiest one

--- src/cifParserC.cpp
+++ src/cifParserC.cpp
@@ -34,6 +34,30 @@ int newsec(FILE *file)
         return 1;
     }
 }
+
+// Helper function to find and return the entry section of the mmCIF
+Rcpp::StringVector entry(FILE *file) {
+    // Read three characters
+    char line[8];
+
+    for (int i = 0; i < 7; i++)
+    {
+        line[i] = fgetc(file);
+    }
+    //line[2] = fgetc(file);
+    // Terminate array
+    line[8] = '\0';
+
+    if (strcmp(line, "_entry.\0") == 0) {
+        printf("%s\n", line);
+        Rcpp::StringVector myvector(1);
+        myvector[0] = line;
+        return myvector;
+    } else {
+        fseek(file, -7, SEEK_CUR);
+        return 1;
+    }
+}
     
 // [[Rcpp::export]]
 List cifParserC(std::string strings="")
@@ -55,40 +79,56 @@ List cifParserC(std::string strings="")
     //*pl = 'b';
     //free(pl);
 
-    int index = 0;
+    //int index = 0;
+
+    fseek(file, 9, SEEK_CUR);
+    line2[0] = fgetc(file);
+    line2[1] = '\0';
+    //printf("%i\n", c);
     int newsection = newsec(file);
+    printf("%i\n", newsection);
+    Rcpp::StringVector line3 = entry(file);
+
+    for (int i = 0; i < 7; i++)
+    {
+        line[i] = fgetc(file);
+    }
+    line[7] = '\0';
+    
 
     // Iterate over the characters of the file
-    for (int c = fgetc(file); c != EOF; c = fgetc(file))
-    {
+    //for (int c = fgetc(file); c != EOF; c = fgetc(file))
+    //do {
 
         // Parse section
-        if (newsection) 
-        {
-            line[index] = c;
-            index++;
-            while ((c = fgetc(file)) != EOF && c != '\n')
-            {
-                //c = fgetc(file);
-                //printf("%c", c);
-                if (index < maxchar)
-                {
-                    line[index] = c;
-                    index++;
-                }
-            }
-        }
-
-        // After newline, check if a new section starts
-        newsection = newsec(file);
-    }
+//        if (newsection) 
+//        {
+//            line[index] = c;
+//            index++;
+//            while ((c = fgetc(file)) != EOF && c != '\n')
+//            {
+//                //c = fgetc(file);
+//                //printf("%c", c);
+//                if (index < maxchar)
+//                {
+//                    line[index] = c;
+//                    index++;
+//                }
+//            }
+//        }
+//
+//        // After newline, check if a new section starts
+//        newsection = newsec(file);
+    //}
+    //    c = fgetc(file);
+    //} while (c != EOF);
 
     // Terminate array
-    line[index] = '\0';
+//    line[index] = '\0';
 
     // Close text
     fclose(file);
 
     // Return output
-    return List::create(_["line"] = line, line2);
+    return List::create(_["line"] = line, line2, line3);
 }
