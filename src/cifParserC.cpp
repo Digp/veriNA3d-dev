b5ca12fb0170add5b121ab0d1b6e2da5628b74c6
Digp <diego.gallego@irbbarcelona.org>
2019-05-21 16:24:20 +0200

    Added some comments

--- src/cifParserC.cpp
+++ src/cifParserC.cpp
@@ -21,8 +21,7 @@ List cifParserC(std::string strings="")
     }
 
     // Define containers for sections 
-    Rcpp::StringVector presec1;
-    Rcpp::StringVector presec2;
+    Rcpp::StringVector tmpsec;
     Rcpp::StringVector sec1;
     Rcpp::StringVector sec2;
 
@@ -34,23 +33,39 @@ List cifParserC(std::string strings="")
         // After newline, check if a new section starts
         c = fgetc(file);
         newsection = newsec(file, c);
+
         // Parse section
         if (newsection) 
         {
+            // Check if it's "_entry" section and parse it
             c = fgetc(file);
-            presec1 = entry(file, c);
-            if (presec1[0] != "") 
+            tmpsec = entry(file, c);
+            if (tmpsec[0] != "") 
             {
-                sec1 = presec1;
+                sec1 = tmpsec;
             }
-            //Rcpp::Rcout << presec1[0];// << '\n';
+            //Rcpp::Rcout << tmpsec[0];// << '\n';
+
+            // Check if it's "_audit_conform" section and parse it
             c = fgetc(file);
-            presec2 = audit_conform(file, c);
-            if (presec2[0] != "") 
+            tmpsec = audit_conform(file, c);
+            if (tmpsec[0] != "") 
             {
-                sec2 = presec2;
+                sec2 = tmpsec;
             }
-            //Rcpp::Rcout << presec2[0];// << '\n';
+
+            // Check if it's "_database_2" section and parse it
+            // Check if it's "_pdbx_database_status" section and parse it
+            // Check if it's "_audit_author" section and parse it
+            // Check if it's "_entity" section and parse it
+            // Check if it's "_chem_comp" section and parse it
+            // Check if it's "_exptl" section and parse it
+            // Check if it's "_struct" section and parse it
+            // Check if it's "_struct_keywords" section and parse it
+            // Check if it's "_struct_asym" section and parse it
+            // Check if it's "_atom_sites" section and parse it
+            // Check if it's "_atom_type" section and parse it
+            // Check if it's "_atom_site" section and parse it
         }
 
         // Iterate over the characters of the file until a newline is found
